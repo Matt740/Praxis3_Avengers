7b4001d22d34dc1496f8f41e1691aad7cb1455d4
Brody Howard <howbro@live.com>
2024-03-06 11:30:39 -0500

    Setting up folders and files
    Just initializing some things

---
+++ Control Subsystem/sensor_library.py
@@ -0,0 +1,60 @@
+from machine import I2C, Pin
+from utime import sleep_ms
+
+# I2C RELEVANT CLASSES
+###############################################################################################################
+#Defines I2C base commands for reading and writing bytes to sensors
+class I2CBase:
+    def writeto_mem(self, addr, memaddr, buf, *, addrsize=8):
+        raise NotImplementedError('writeto_mem')
+
+    def readfrom_mem(self, addr, memaddr, nbytes, *, addrsize=8):
+        raise NotImplementedError('readfrom_mem')
+
+    def write8(self, addr, buf, stop=True):
+        raise NotImplementedError('write')
+
+    def read16(self, addr, nbytes, stop=True):
+        raise NotImplementedError('read')
+
+    def __init__(self, bus=None, freq=None, sda=None, scl=None):
+        raise NotImplementedError('__init__')
+
+#Class which defines the I2C connection and which pins are being used using. Uses I2C base commands.
+class I2CUnifiedMachine(I2CBase):
+    def __init__(self, bus=None, freq=None, sda=None, scl=None):
+        if freq is None: freq = 400_000
+        if not isinstance(freq, (int)):
+            raise ValueError("freq must be an Int")
+        if freq < 400_000: print("\033[91mWarning: minimum freq 400kHz is recommended if using OLED module.\033[0m")
+        if bus is not None and sda is not None and scl is not None:
+            print('Using supplied bus, sda, and scl to create machine.I2C() with freq: {} Hz'.format(freq))
+            self.i2c = I2C(bus, freq=freq, sda=sda, scl=scl)
+        elif bus is None and sda is None and scl is None:
+            self.i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=freq) # RPI Pico in Expansion Board
+        else:
+            raise Exception("Please provide at least bus, sda, and scl")
+
+        self.writeto_mem = self.i2c.writeto_mem
+        self.readfrom_mem = self.i2c.readfrom_mem
+
+    def write8(self, addr, reg, data):
+        if reg is None:
+            self.i2c.writeto(addr, data)
+        else:
+            self.i2c.writeto(addr, reg + data)
+            
+    def read16(self, addr, reg):
+        self.i2c.writeto(addr, reg, False)
+        return self.i2c.readfrom(addr, 2)
+        
+    def scan(self):
+        print([hex(i) for i in self.i2c.scan()])
+
+# Overall function to create 12C object and returns the object 
+def create_unified_i2c(bus=None, freq=None, sda=None, scl=None, suppress_warnings=True):
+    i2c = I2CUnifiedMachine(bus=bus, freq=freq, sda=sda, scl=scl)
+    return i2c
+
+###############################################################################################################################
+
