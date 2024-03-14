ba9e2f90230a98aec7f4d26f91e6766da28fd8d9
Brody Howard <howbro@live.com>
2024-03-13 23:15:35 -0400

    Getting ready for testing Accelerometer

--- Control Subsystem/Accelerometer_testing.py
+++ Control Subsystem/Accelerometer_testing.py
@@ -1,9 +1,8 @@
 import time
-from PI4_sensor_library import BME680, QwiicKX13X, L76B
+from PI4_sensor_library import QwiicKX13X
 
 Accelerometer = QwiicKX13X(1)
 i2c_network = Accelerometer.i2c
-Barometer = BME680(1, i2c_network)
 
 Accelerometer.initialize(True, False, 8, False, False)
 print(Accelerometer.get_accel_data())
