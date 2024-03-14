d70619d00dcb4d8604a1f0c144a2b5cd3a8e3126
Brody Howard <howbro@live.com>
2024-03-14 02:06:47 -0400

    Update BME680_Testing.py
    Just making acccurate to what was used during testing

--- Control Subsystem/BME680_Testing.py
+++ Control Subsystem/BME680_Testing.py
@@ -1,8 +1,9 @@
 import time
 from PI4_sensor_library import BME680
 
-Bme = BME680(1, sea_level_pressure=1000)
+Bme = BME680(1, sea_level_pressure=1004.85)
 
 while True:
-    print(Bme.get_altitude())
+    print('Altitude =', Bme.get_altitude(), 'm')
+    print('Pressure =', Bme.data.pressure, 'hpa')
     time.sleep(2)
