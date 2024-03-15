f309ba2054f6e5372e4c5766d0dcfa60f8b8d8cf
Brody Howard <howbro@live.com>
2024-03-15 13:59:42 -0400

    Update GPS L76B Testing.py
    Getting Gps testing code ready

--- Control Subsystem/GPS L76B Testing.py
+++ Control Subsystem/GPS L76B Testing.py
@@ -4,6 +4,11 @@ from PI4_sensor_library import L76B
 Standby_Pin = 14
 Force_Pin = 17
 
+BAUDRATE = 9600
+gps = L76B(Standby_Pin, Force_Pin)
+gps.l76x_exit_backup_mode()
+gps.l76x_send_command(gps.SET_SYNC_PPS_NMEA_ON)
+
 while True:
-    
-    time.sleep(2)
+    print(chr(gps.uart_receive_byte()[0]),end="")
+    time.sleep(3)
