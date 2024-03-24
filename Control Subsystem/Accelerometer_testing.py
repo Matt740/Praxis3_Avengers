19b019d6e1145a480954f5578ffcc4fbe618ec02
Brody Howard <howbro@live.com>
2024-03-24 19:34:32 -0400

    Update Accelerometer_testing.py

--- Control Subsystem/Accelerometer_testing.py
+++ Control Subsystem/Accelerometer_testing.py
@@ -1,12 +1,24 @@
 import time
+#import our sensor library
 from PI4_sensor_library import QwiicKX13X
 
+#Set up the accelerometer class and it will also set up the i2c bus 
 Accelerometer = QwiicKX13X(1)
-i2c_network = Accelerometer.i2c
 
-Accelerometer.initialize(True, False, 8, False, False)
-print(Accelerometer.get_g_range())
+#Send values to configuration registers for the inital settings for measurement we want
+Accelerometer.initialize(True, False, 2, False, False)
+#Read value in g range cnfiguration register to see if it is the one we previously set
+g_range = Accelerometer.get_g_range()
+print(g_range)
+if g_range == 2: print("Succesful I2C Read and Write")
 
+# Prefrom hardware self check to test that accelerometer is giving accurate readings
+if Accelerometer.run_command_test() == True:
+    print("Succeful Hardware Self Test")
+else:
+    print("Unceccesful Hardware Self Test")
+
+#Print acceleration data readings 
 for i in range(10):
     x_accel, y_accel, z_accel = Accelerometer.get_accel_data()
     print(f'Acceleration X: {x_accel}, y: {y_accel}, z: {z_accel}')
