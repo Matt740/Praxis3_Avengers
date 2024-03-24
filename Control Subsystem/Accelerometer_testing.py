23804a1a9f24e76a0686f2f3e796517e6e4776c7
Brody Howard <howbro@live.com>
2024-03-24 19:54:38 -0400

    Update Accelerometer_testing.py

--- Control Subsystem/Accelerometer_testing.py
+++ Control Subsystem/Accelerometer_testing.py
@@ -13,13 +13,15 @@ print(g_range)
 if g_range == 2: print("Succesful I2C Read and Write")
 
 # Prefrom hardware self check to test that accelerometer is giving accurate readings
-if Accelerometer.run_command_test() == True:
+if Accelerometer.run_command_test():
     print("Succeful Hardware Self Test")
 else:
     print("Unceccesful Hardware Self Test")
 
+time.sleep(4)
+
 #Print acceleration data readings 
-for i in range(10):
+while True:
     x_accel, y_accel, z_accel = Accelerometer.get_accel_data()
     print(f'Acceleration X: {x_accel}, y: {y_accel}, z: {z_accel}')
-    time.sleep(2)
+    time.sleep(0.5)
